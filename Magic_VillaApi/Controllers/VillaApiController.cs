6ec168615e0a26b0fad9dfe093ad4ab8a4eac128
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-02-24 16:14:22 +0530

    Adding Api Controller & Routing it.

---
+++ Magic_VillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,20 @@
+﻿using Magic_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_VillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase // Controller created
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() // this is end point
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 2,Name = "Beach View" }
+            };
+        }
+    }
+}
---
+++ Magic_VillaApi/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace Magic_VillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
